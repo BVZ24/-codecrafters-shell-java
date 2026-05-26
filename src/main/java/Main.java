ac6510b36555f6c11816029eafc96c873a4927fa
Byron Velez <byron24velez@gmail.com>
2026-05-25 21:19:06 -0400

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,9 +17,14 @@ public class Main {
                 break;
 
             } else if (input.startsWith("echo ")) {
-		System.out.println(input.substring(5));
-	    } else {
-		System.out.println(input + ": command not found");
+
+                System.out.println(input.substring(5));
+
+            } else {
+
+                System.out.println(input + ": command not found");
+
+
 	    }
 
             System.out.println(input + ": command not found");
