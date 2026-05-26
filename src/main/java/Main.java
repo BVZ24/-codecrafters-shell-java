2aaff4f36c6baa6915f68c06c9f2bb7f4fef7d08
Byron Velez <byron24velez@gmail.com>
2026-05-25 21:09:32 -0400

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,22 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-0
+
         Scanner scanner = new Scanner(System.in);
-        0000
 
         while (true) {
 
             System.out.print("$ ");
 
             String input = scanner.nextLine();
-	     if (input.equals("exit")) {
+
+            if (input.equals("exit")) {
 
                 break;
 
             }
-
-
 
             System.out.println(input + ": command not found");
 
@@ -26,6 +24,6 @@ public class Main {
 
     }
 
-}
+
 
 
