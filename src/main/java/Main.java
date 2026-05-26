f656a55336d2bec488689eeddb4045c913784735
Byron Velez <byron24velez@gmail.com>
2026-05-25 21:03:54 -0400

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
+        if (input.equals("exit"){
+	    break;
+	}
 
         while (true) {
 
